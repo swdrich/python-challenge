ac84868b4b76aaac52ef6684ca70d40cddb7b882
Steph <swdrinkard@gmail.com>
2021-01-18 09:09:52 -0600

    Added code to calculate changes month over month.

--- PyBank/main.py
+++ PyBank/main.py
@@ -16,23 +16,46 @@ with open(csv_path) as csv_file:
     #print(header)
     
     #define starting variables
-    row_count=0
     net_total=0
+    months = []
+    values = []
     
     #begin the for loop
     for row in budget_data:
-        #print(row)
-        row_count = row_count + 1
-        #print(row[1])
+        
+        #add items to lists
+        months.append(str(row[0]))
+        values.append(int(row[1]))
+        #print(months)
+        #print(values)
    
+        #Calculate net total:
         #read value from row[1] and cast as int
         monthly_net = int(row[1])
         #add amounts
         net_total = net_total + monthly_net
    
+       
+#calculate total number of months
+total_months = len(months)
 
-    print(f"Total Months: {row_count}")
-    print(f"Total: ${net_total}")
+#calculate monthly value change and store to new list
+
+value_change = []
+for num in values:
+    previous_num = values[values.index(num)-1]
+    change_amount = num - previous_num
+    #print(change_amount)
+    value_change.append(int(change_amount))
+
+#print(value_change)
+
+
+
+print("Financial Analysis")
+print("--------------------------")
+print(f"Total Months: {total_months}")
+print(f"Total: ${net_total}")
 
         #compare each month to the one before it,
         #calculate the difference, store as variable
